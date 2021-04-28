55d991e715126a859bc7b17c83b97036e66d7e50
Emma Waffle <emma@waffle.coffee>
2021-04-28 12:47:39 -0400

    Rewrite of the majority of the project Reshifting of focus from pride quilts to pride flags, since apparently there isn't a mod for that already Pretty much the only thing that needs to be done are the textures. New icon too!

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Pride Quilts
+# Pride Flags
 
 Barely actually does anything :P My first solo Fabric mod... so nothing will probably work :D
 
 Copyright Emmaffle <pridequilts@waffle.coffee> 2021. Distributed under the Boost Software License, Version 1.0 ("BSL-1.0").  
-(See accompanying file 'LICENSE' or copy at https://www.boost.org/LICENSE_1_0.txt)
+(See accompanying file 'LICENSE' or copy at https://www.boost.org/LICENSE_1_0.txt )
---
+++ src/main/java/coffee/waffle/prideflags/PrideFlags.java
@@ -0,0 +1,66 @@
+package coffee.waffle.prideflags;
+
+/*
+ Copyright Emmaffle <pridequilts@waffle.coffee> 2021. Distributed under the Boost Software License, Version 1.0.
+ (See accompanying file 'LICENSE' or copy at https://www.boost.org/LICENSE_1_0.txt)
+*/
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.client.item.TooltipContext;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.world.World;
+
+import java.util.List;
+
+public class PrideFlags implements ModInitializer {
+    public static final Item RAINBOW_FLAG = new Item(new FabricItemSettings().group(ItemGroup.MISC));
+    public static final Item TRANS_FLAG = new Item(new FabricItemSettings().group(ItemGroup.MISC));
+    public static final Item NONBINARY_FLAG = new Item(new FabricItemSettings().group(ItemGroup.MISC));
+    public static final Item GENDERFLUID_FLAG = new Item(new FabricItemSettings().group(ItemGroup.MISC));
+    public static final Item BI_FLAG = new Item(new FabricItemSettings().group(ItemGroup.MISC));
+    public static final Item LESBIAN_FLAG = new Item(new FabricItemSettings().group(ItemGroup.MISC));
+    public static final Item ACE_FLAG = new Item(new FabricItemSettings().group(ItemGroup.MISC));
+    public static final Item ARO_FLAG = new Item(new FabricItemSettings().group(ItemGroup.MISC));
+    public static final Item PAN_FLAG = new Item(new FabricItemSettings().group(ItemGroup.MISC));
+    public static final Item POLY_FLAG = new Item(new FabricItemSettings().group(ItemGroup.MISC));
+    public static final Item AGENDER_FLAG = new Item(new FabricItemSettings().group(ItemGroup.MISC));
+    public static final Item MLM_FLAG = new Item(new FabricItemSettings().group(ItemGroup.MISC));
+
+    @Override
+    public void onInitialize() {
+        Registry.register(Registry.ITEM, new Identifier("prideflags", "rainbow_flag"), RAINBOW_FLAG);
+        Registry.register(Registry.ITEM, new Identifier("prideflags", "trans_flag"), TRANS_FLAG);
+        Registry.register(Registry.ITEM, new Identifier("prideflags", "nonbinary_flag"), NONBINARY_FLAG);
+        Registry.register(Registry.ITEM, new Identifier("prideflags", "genderfluid_flag"), GENDERFLUID_FLAG);
+        Registry.register(Registry.ITEM, new Identifier("prideflags", "bi_flag"), BI_FLAG);
+        Registry.register(Registry.ITEM, new Identifier("prideflags", "lesbian_flag"), LESBIAN_FLAG);
+        Registry.register(Registry.ITEM, new Identifier("prideflags", "ace_flag"), ACE_FLAG);
+        Registry.register(Registry.ITEM, new Identifier("prideflags", "aro_flag"), ARO_FLAG);
+        Registry.register(Registry.ITEM, new Identifier("prideflags", "pan_flag"), PAN_FLAG);
+        Registry.register(Registry.ITEM, new Identifier("prideflags", "poly_flag"), POLY_FLAG);
+        Registry.register(Registry.ITEM, new Identifier("prideflags", "agender_flag"), AGENDER_FLAG);
+        Registry.register(Registry.ITEM, new Identifier("prideflags", "mlm_flag"), MLM_FLAG);
+    }
+
+    public void appendTooltip(ItemStack itemStack, World world, List<Text> tooltip, TooltipContext tooltipContext) {
+        tooltip.add(new TranslatableText("item.prideflags.rainbow_flag"));
+        tooltip.add(new TranslatableText("item.prideflags.trans_flag"));
+        tooltip.add(new TranslatableText("item.prideflags.nonbinary_flag"));
+        tooltip.add(new TranslatableText("item.prideflags.genderfluid_flag"));
+        tooltip.add(new TranslatableText("item.prideflags.bi_flag"));
+        tooltip.add(new TranslatableText("item.prideflags.lesbian_flag"));
+        tooltip.add(new TranslatableText("item.prideflags.ace_flag"));
+        tooltip.add(new TranslatableText("item.prideflags.aro_flag"));
+        tooltip.add(new TranslatableText("item.prideflags.pan_flag"));
+        tooltip.add(new TranslatableText("item.prideflags.poly_flag"));
+        tooltip.add(new TranslatableText("item.prideflags.agender_flag"));
+        tooltip.add(new TranslatableText("item.prideflags.mlm_flag"));
+    }
+}
