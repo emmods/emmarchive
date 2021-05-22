bfdfc75580d59bb744268d5d076e912e47244f2f
Emma Waffle <emma@waffle.coffee>
2021-05-22 17:56:20 -0400

    cleanup a bit

--- src/main/java/coffee/waffle/prideflags/PrideFlags.java
+++ src/main/java/coffee/waffle/prideflags/PrideFlags.java
@@ -1,98 +1,30 @@
-package coffee.waffle.prideflags;
-
 /*
  Copyright Emmaffle <pridemcmod@waffle.coffee> 2021. Distributed under the Boost Software License, Version 1.0.
  (See accompanying file 'LICENSE' or copy at https://www.boost.org/LICENSE_1_0.txt)
 */
 
+package coffee.waffle.prideflags;
+
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
-import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
-import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
-import net.minecraft.client.item.TooltipContext;
-import net.minecraft.item.BlockItem;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
-import net.minecraft.text.Text;
-import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
-import net.minecraft.world.World;
-
-import java.util.List;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class PrideFlags implements ModInitializer {
-    public static final Item RAINBOW_FLAG = new Item(new FabricItemSettings().group(PrideFlags.PRIDE_GROUP));
-    public static final ItemGroup PRIDE_GROUP = FabricItemGroupBuilder.build(
-            new Identifier("prideflags", "flags"),
-            () -> new ItemStack(PrideFlags.RAINBOW_FLAG));
-    public static final Item TRANS_FLAG = new Item(new FabricItemSettings().group(PrideFlags.PRIDE_GROUP));
-    public static final Item NONBINARY_FLAG = new Item(new FabricItemSettings().group(PrideFlags.PRIDE_GROUP));
-    public static final Item GENDERFLUID_FLAG = new Item(new FabricItemSettings().group(PrideFlags.PRIDE_GROUP));
-    public static final Item BI_FLAG = new Item(new FabricItemSettings().group(PrideFlags.PRIDE_GROUP));
-    public static final Item LESBIAN_FLAG = new Item(new FabricItemSettings().group(PrideFlags.PRIDE_GROUP));
-    public static final Item ACE_FLAG = new Item(new FabricItemSettings().group(PrideFlags.PRIDE_GROUP));
-    public static final Item ARO_FLAG = new Item(new FabricItemSettings().group(PrideFlags.PRIDE_GROUP));
-    public static final Item PAN_FLAG = new Item(new FabricItemSettings().group(PrideFlags.PRIDE_GROUP));
-    public static final Item POLY_FLAG = new Item(new FabricItemSettings().group(PrideFlags.PRIDE_GROUP));
-    public static final Item AGENDER_FLAG = new Item(new FabricItemSettings().group(PrideFlags.PRIDE_GROUP));
-    public static final Item MLM_FLAG = new Item(new FabricItemSettings().group(PrideFlags.PRIDE_GROUP));
+  public static final String MODID = "prideflags";
+  public static final Logger LOGGER = LogManager.getLogger(MODID);
 
-    public static final Block RAINBOW_BLOCK = new Block(FabricBlockSettings.copy(Blocks.WHITE_WOOL));
-    public static final Block TRANS_BLOCK = new Block(FabricBlockSettings.copy(Blocks.WHITE_WOOL));
-    public static final Block NONBINARY_BLOCK = new Block(FabricBlockSettings.copy(Blocks.WHITE_WOOL));
-    public static final Block GENDERFLUID_BLOCK = new Block(FabricBlockSettings.copy(Blocks.WHITE_WOOL));
-    public static final Block BI_BLOCK = new Block(FabricBlockSettings.copy(Blocks.WHITE_WOOL));
-    public static final Block LESBIAN_BLOCK = new Block(FabricBlockSettings.copy(Blocks.WHITE_WOOL));
-    public static final Block ACE_BLOCK = new Block(FabricBlockSettings.copy(Blocks.WHITE_WOOL));
-    public static final Block ARO_BLOCK = new Block(FabricBlockSettings.copy(Blocks.WHITE_WOOL));
-    public static final Block PAN_BLOCK = new Block(FabricBlockSettings.copy(Blocks.WHITE_WOOL));
-    public static final Block POLY_BLOCK = new Block(FabricBlockSettings.copy(Blocks.WHITE_WOOL));
-    public static final Block AGENDER_BLOCK = new Block(FabricBlockSettings.copy(Blocks.WHITE_WOOL));
-    public static final Block MLM_BLOCK = new Block(FabricBlockSettings.copy(Blocks.WHITE_WOOL));
+  public static final ItemGroup PRIDE_GROUP = FabricItemGroupBuilder.build(new Identifier("prideflags", "flags"), () -> new ItemStack(Register.RAINBOW_FLAG));
 
-    @Override
-    public void onInitialize() {
-        Registry.register(Registry.ITEM, new Identifier("prideflags", "rainbow_flag"), RAINBOW_FLAG);
-        Registry.register(Registry.ITEM, new Identifier("prideflags", "trans_flag"), TRANS_FLAG);
-        Registry.register(Registry.ITEM, new Identifier("prideflags", "nonbinary_flag"), NONBINARY_FLAG);
-        Registry.register(Registry.ITEM, new Identifier("prideflags", "genderfluid_flag"), GENDERFLUID_FLAG);
-        Registry.register(Registry.ITEM, new Identifier("prideflags", "bi_flag"), BI_FLAG);
-        Registry.register(Registry.ITEM, new Identifier("prideflags", "lesbian_flag"), LESBIAN_FLAG);
-        Registry.register(Registry.ITEM, new Identifier("prideflags", "ace_flag"), ACE_FLAG);
-        Registry.register(Registry.ITEM, new Identifier("prideflags", "aro_flag"), ARO_FLAG);
-        Registry.register(Registry.ITEM, new Identifier("prideflags", "pan_flag"), PAN_FLAG);
-        Registry.register(Registry.ITEM, new Identifier("prideflags", "poly_flag"), POLY_FLAG);
-        Registry.register(Registry.ITEM, new Identifier("prideflags", "agender_flag"), AGENDER_FLAG);
-        Registry.register(Registry.ITEM, new Identifier("prideflags", "mlm_flag"), MLM_FLAG);
+  @Override
+  public void onInitialize() {
+    Register.registerFlagItems();
+    Register.registerPrideBlocks();
+    Register.registerPrideBlockitems();
 
-        Registry.register(Registry.BLOCK, new Identifier("prideflags", "rainbow_block"), RAINBOW_BLOCK);
-        Registry.register(Registry.BLOCK, new Identifier("prideflags", "trans_block"), TRANS_BLOCK);
-        Registry.register(Registry.BLOCK, new Identifier("prideflags", "nonbinary_block"), NONBINARY_BLOCK);
-        Registry.register(Registry.BLOCK, new Identifier("prideflags", "genderfluid_block"), GENDERFLUID_BLOCK);
-        Registry.register(Registry.BLOCK, new Identifier("prideflags", "bi_block"), BI_BLOCK);
-        Registry.register(Registry.BLOCK, new Identifier("prideflags", "lesbian_block"), LESBIAN_BLOCK);
-        Registry.register(Registry.BLOCK, new Identifier("prideflags", "ace_block"), ACE_BLOCK);
-        Registry.register(Registry.BLOCK, new Identifier("prideflags", "aro_block"), ARO_BLOCK);
-        Registry.register(Registry.BLOCK, new Identifier("prideflags", "pan_block"), PAN_BLOCK);
-        Registry.register(Registry.BLOCK, new Identifier("prideflags", "poly_block"), POLY_BLOCK);
-        Registry.register(Registry.BLOCK, new Identifier("prideflags", "agender_block"), AGENDER_BLOCK);
-        Registry.register(Registry.BLOCK, new Identifier("prideflags", "mlm_block"), MLM_BLOCK);
-
-        Registry.register(Registry.ITEM, new Identifier("prideflags", "rainbow_block"), new BlockItem(RAINBOW_BLOCK, new FabricItemSettings().group(PrideFlags.PRIDE_GROUP)));
-        Registry.register(Registry.ITEM, new Identifier("prideflags", "trans_block"), new BlockItem(TRANS_BLOCK, new FabricItemSettings().group(PrideFlags.PRIDE_GROUP)));
-        Registry.register(Registry.ITEM, new Identifier("prideflags", "nonbinary_block"), new BlockItem(NONBINARY_BLOCK, new FabricItemSettings().group(PrideFlags.PRIDE_GROUP)));
-        Registry.register(Registry.ITEM, new Identifier("prideflags", "genderfluid_block"), new BlockItem(GENDERFLUID_BLOCK, new FabricItemSettings().group(PrideFlags.PRIDE_GROUP)));
-        Registry.register(Registry.ITEM, new Identifier("prideflags", "bi_block"), new BlockItem(BI_BLOCK, new FabricItemSettings().group(PrideFlags.PRIDE_GROUP)));
-        Registry.register(Registry.ITEM, new Identifier("prideflags", "lesbian_block"), new BlockItem(LESBIAN_BLOCK, new FabricItemSettings().group(PrideFlags.PRIDE_GROUP)));
-        Registry.register(Registry.ITEM, new Identifier("prideflags", "ace_block"), new BlockItem(ACE_BLOCK, new FabricItemSettings().group(PrideFlags.PRIDE_GROUP)));
-        Registry.register(Registry.ITEM, new Identifier("prideflags", "aro_block"), new BlockItem(ARO_BLOCK, new FabricItemSettings().group(PrideFlags.PRIDE_GROUP)));
-        Registry.register(Registry.ITEM, new Identifier("prideflags", "pan_block"), new BlockItem(PAN_BLOCK, new FabricItemSettings().group(PrideFlags.PRIDE_GROUP)));
-        Registry.register(Registry.ITEM, new Identifier("prideflags", "poly_block"), new BlockItem(POLY_BLOCK, new FabricItemSettings().group(PrideFlags.PRIDE_GROUP)));
-        Registry.register(Registry.ITEM, new Identifier("prideflags", "agender_block"), new BlockItem(AGENDER_BLOCK, new FabricItemSettings().group(PrideFlags.PRIDE_GROUP)));
-        Registry.register(Registry.ITEM, new Identifier("prideflags", "mlm_block"), new BlockItem(MLM_BLOCK, new FabricItemSettings().group(PrideFlags.PRIDE_GROUP)));
-    }
+    LOGGER.info("Pride Flags has finished loading! \uD83C\uDFF3️\u200D\uD83C\uDF08");
+  }
 }
