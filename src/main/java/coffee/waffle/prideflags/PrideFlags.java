9ca4c2b9f05185b6f2430634b9817793d0769049
Emma Waffle <emma@waffle.coffee>
2021-05-31 20:09:25 -0400

    Update to 1.17-pre2, Java 16 Fuck it, ship it

--- src/main/java/coffee/waffle/prideflags/PrideFlags.java
+++ src/main/java/coffee/waffle/prideflags/PrideFlags.java
@@ -6,18 +6,12 @@
 package coffee.waffle.prideflags;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.Identifier;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class PrideFlags implements ModInitializer {
   public static final String MODID = "prideflags";
   public static final Logger LOGGER = LogManager.getLogger(MODID);
-
-  public static final ItemGroup PRIDE_GROUP = FabricItemGroupBuilder.build(new Identifier("prideflags", "flags"), () -> new ItemStack(PrideFlagsRegistry.RAINBOW_FLAG));
 
   @Override
   public void onInitialize() {
